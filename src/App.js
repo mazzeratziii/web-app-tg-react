f525552b4ab0c5dd28a4fade1caa8c5c0953b29d
mazzeratziii <grauxxx1@yandex.ru>
2023-10-16 17:37:52 +0500

    Merge remote-tracking branch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import {useEffect} from "react";
-import {useTelegram} from "./hooks/useTelegram";
+// import {useTelegram} from "./hooks/useTelegram";
 import Header from "./components/Header/Header";
+import {Route,Routes} from 'react-router-dom';
+import ProductList from "./components/ProductList/ProductList";
+import Form from './components/Form/Form';
 const tg = window.Telegram.WebApp;
 
 
+
 function App() {
-    const {onToggleButton} = useTelegram();
+
 
     useEffect(() => {
         tg.ready();
@@ -15,7 +19,10 @@ function App() {
     return (
         <div className="App">
             <Header />
-        <button onClick={onToggleButton}>toggle</button>
+            <Routes>
+                <Route index element={<ProductList/>}/>
+                <Route path={'form'} element={<Form/>}/>
+            </Routes>
     </div>
   );
 }
--- src/hooks/useTelegram.js
+++ src/hooks/useTelegram.js
@@ -14,7 +14,7 @@ export function useTelegram(){
     return{
         onClose,
         onToggleButton,
-    tg,
+        tg,
         user: tg.initDataUnsafe?.user,
     }
 }
