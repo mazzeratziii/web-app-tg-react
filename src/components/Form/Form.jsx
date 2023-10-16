bb722d613a987d60139ae152496f494fe7273fb7
mazzeratziii <grauxxx1@yandex.ru>
2023-10-16 18:08:17 +0500

    Merge remote-tracking branch 'origin/master'

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,25 +1,42 @@
-import React, {useEffect, useState} from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import './Form.css';
 import {useTelegram} from "../../hooks/useTelegram";
+
 const Form = () => {
-    const  [country,setCountry] = useState('');
-    const  [street,setStreet] = useState('');
-    const  [subject,setSubject] = useState('physical');
+    const [country, setCountry] = useState('');
+    const [street, setStreet] = useState('');
+    const [subject, setSubject] = useState('physical');
     const {tg} = useTelegram();
 
+    const onSendData = useCallback(() => {
+        const data = {
+            country,
+            street,
+            subject
+        }
+        tg.sendData(JSON.stringify(data));
+    }, [country, street, subject])
+
     useEffect(() => {
-       if(!street || !country){
-            tg.MainButton.hide();
-       } else {
-           tg.MainButton.show();
-       }
-    }, [country,street])
+        tg.onEvent('mainButtonClicked', onSendData)
+        return () => {
+            tg.offEvent('mainButtonClicked', onSendData)
+        }
+    }, [onSendData])
 
     useEffect(() => {
         tg.MainButton.setParams({
-            text:'Отправить данные'
+            text: 'Отправить данные'
         })
     }, [])
+
+    useEffect(() => {
+        if(!street || !country) {
+            tg.MainButton.hide();
+        } else {
+            tg.MainButton.show();
+        }
+    }, [country, street])
 
     const onChangeCountry = (e) => {
         setCountry(e.target.value)
@@ -37,18 +54,18 @@ const Form = () => {
         <div className={"form"}>
             <h3>Введите ваши данные</h3>
             <input
-                className= {'input'}
-                   type="text"
-                   placeholder={'Страна'}
-                   value={country}
-                   onChange={onChangeCountry}
+                className={'input'}
+                type="text"
+                placeholder={'Страна'}
+                value={country}
+                onChange={onChangeCountry}
             />
             <input
-                   className={'input'}
-                   type="text"
-                   placeholder={'Улица'}
-                    value={street}
-                    onChange={onChangeStreet}
+                className={'input'}
+                type="text"
+                placeholder={'Улица'}
+                value={street}
+                onChange={onChangeStreet}
             />
             <select value={subject} onChange={onChangeSubject} className={'select'}>
                 <option value={'physical'}>Физ. лицо</option>
