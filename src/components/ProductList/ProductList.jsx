60e247bbbbe1b7d1966ff01e5516704ba0ef4dfa
mazzeratziii <grauxxx1@yandex.ru>
2023-10-21 21:42:28 +0500

    Merge remote-tracking branch 'origin/master'

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -15,23 +15,14 @@ const products = [
     {id: '8', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая'},
 ]
 
-const getTotalPrice = (items = []) => {
-    return items.reduce((acc, item) => {
 
-        return acc += item.price
-    }, 0)
-}
 
 const ProductList = () => {
     const [addedItems, setAddedItems] = useState([]);
-    const {tg, queryId} = useTelegram();
+    const {tg, } = useTelegram();
 
     const onSendData = useCallback(() => {
-        const data = {
-            products: addedItems,
-            totalPrice: getTotalPrice(addedItems),
-            queryId,
-        }
+
 
     }, [addedItems])
 
@@ -51,7 +42,12 @@ const ProductList = () => {
         } else {
             newItems = [...addedItems, product];
         }
+        const getTotalPrice = (items = []) => {
+            return items.reduce((acc, item) => {
 
+                return acc += item.price
+            }, 0)
+        }
         setAddedItems(newItems)
 
         if(newItems.length === 0) {
