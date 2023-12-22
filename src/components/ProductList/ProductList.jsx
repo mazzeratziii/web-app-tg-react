16c5293cc42a2c4adfb2e8d761a6a7753fcf70fe
Dima <grauxxx1@yandex.ru>
2023-12-22 13:39:40 +0500

    Merge remote-tracking branch 'origin/master'

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,83 +1,84 @@
-import React, {useState} from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import './ProductList.css';
 import ProductItem from "../ProductItem/ProductItem";
-import {useTelegram} from "../../hooks/useTelegram";
-import {useCallback, useEffect} from "react";
+import { useTelegram } from "../../hooks/useTelegram";
 
 const products = [
-    {id: '1', title: 'Джинсы', price: 5000, description: 'Синего цвета, прямые'},
-    {id: '2', title: 'Куртка', price: 12000, description: 'Зеленого цвета, теплая'},
-    {id: '3', title: 'Джинсы 2', price: 5000, description: 'Синего цвета, прямые'},
-    {id: '4', title: 'Куртка 8', price: 122, description: 'Зеленого цвета, теплая'},
-    {id: '5', title: 'Джинсы 3', price: 5000, description: 'Синего цвета, прямые'},
-    {id: '6', title: 'Куртка 7', price: 600, description: 'Зеленого цвета, теплая'},
-    {id: '7', title: 'Джинсы 4', price: 5500, description: 'Синего цвета, прямые'},
-    {id: '8', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая'},
-]
+    // Your product data here
+];
 
 const getTotalPrice = (items = []) => {
-    return items.reduce((acc, item) => {
-        return acc += item.price
-    }, 0)
-}
+    return items.reduce((acc, item) => acc + item.price, 0);
+};
 
 const ProductList = () => {
     const [addedItems, setAddedItems] = useState([]);
-    const {tg, queryId} = useTelegram();
+    const { tg, queryId } = useTelegram();
 
-    const onSendData = useCallback(() => {
+    const sendDataToMainButton = useCallback(() => {
         const data = {
             products: addedItems,
             totalPrice: getTotalPrice(addedItems),
             queryId,
-        }
+        };
+
         fetch('http://92.255.67.216:8000/web-data', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify(data)
+            body: JSON.stringify(data),
         })
-    }, [addedItems])
+            .then(response => {
+                // Handle the response if needed
+                console.log('Data sent successfully!', response);
+            })
+            .catch(error => {
+                // Handle error if the request fails
+                console.error('Error sending data:', error);
+            });
+    }, [addedItems, queryId]);
+
+    const onMainButtonClick = useCallback(() => {
+        sendDataToMainButton();
+        // Additional logic to handle MainButton click if needed
+    }, [sendDataToMainButton]);
 
     useEffect(() => {
-        tg.onEvent('mainButtonClicked', onSendData)
+        tg.onEvent('mainButtonClicked', onMainButtonClick);
+
         return () => {
-            tg.offEvent('mainButtonClicked', onSendData)
-        }
-    }, [onSendData])
+            tg.offEvent('mainButtonClicked', onMainButtonClick);
+        };
+    }, [tg, onMainButtonClick]);
 
     const onAdd = (product) => {
         const alreadyAdded = addedItems.find(item => item.id === product.id);
         let newItems = [];
 
-        if(alreadyAdded) {
+        if (alreadyAdded) {
             newItems = addedItems.filter(item => item.id !== product.id);
         } else {
             newItems = [...addedItems, product];
         }
 
-        let isShow = false;
+        setAddedItems(newItems);
 
-        setAddedItems(newItems)
-
-        if(newItems.length === 0) {
+        if (newItems.length === 0) {
             tg.MainButton.hide();
         } else {
-            isShow = true
             tg.MainButton.show();
             tg.MainButton.setParams({
                 text: `Купить ${getTotalPrice(newItems)}`
             });
         }
-
-
-    }
+    };
 
     return (
         <div className={'list'}>
             {products.map(item => (
                 <ProductItem
+                    key={item.id}
                     product={item}
                     onAdd={onAdd}
                     className={'item'}
@@ -88,5 +89,3 @@ const ProductList = () => {
 };
 
 export default ProductList;
-
-
